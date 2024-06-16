e9d7b258871cef72dc856211a3071d1307e531ae
TuanLam123456 <dantethegodslayer2000@gmail.com>
2024-06-16 19:08:34 +0700

    ProfilePage

---
+++ src/Components/Pages/ProfilePage/ScheduleProfile/ScheduleProfile.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import './../TutorProfilePage/TutorProfilePage.css';
+import ProfileSideBar from '../ProfileSideBar/ProfileSideBar';
+import classroom_image from 'C:/Users/dante/OneDrive/Desktop/Project/demand-tutor/src/Components/Assets/classroom.png';
+import teacher_image from 'C:/Users/dante/OneDrive/Desktop/Project/demand-tutor/src/Components/Assets/teacher.png';
+
+function ScheduleProfile() {
+    const renderCheckboxes = () => {
+        const checkboxes = [];
+        for (let i = 1; i <= 24; i++) {
+            checkboxes.push(
+                <div key={i} className="checkbox-item">
+                    <input type="checkbox" id={`hour-${i}`} name={`hour-${i}`} />
+                    <label htmlFor={`hour-${i}`}>{`${i}:00`}</label>
+                </div>
+            );
+        }
+        return checkboxes;
+    };
+
+    return (
+        <div>
+            <div className="tutor-profile-container">
+                <ProfileSideBar />
+                <div className="tutor-profile-content">
+                    <div className="tutor-fixed-row">
+                        <div className="tutor-image-container">
+                            <img src={classroom_image} alt="Classroom" className="classroom-profile-image" />
+                        </div>
+                    </div>
+                    <div className="tutor-row">
+                        <div className="tutor-profile">
+                            <img src={teacher_image} alt="Classroom" className="tutor-profile-image rounded-image" />
+                        </div>
+                        <div className='tutor-profile-details'>
+                            <h3>Alexa Rowles</h3>
+                            <p>Email: alexa.rowles@example.com</p>
+                        </div>
+                    </div>
+                    <div className="tutor-toggle-container">
+                        <div className="tutor-toggle">
+                            <label className="toggle-label">Be Tutor</label>
+                            <label className="switch">
+                                <input type="checkbox" />
+                                <span className="slider"></span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="hour-checkboxes-container">
+                        {renderCheckboxes()}
+                    </div>
+                    <div className="introduce-yourself-container">
+                        <h3>Introduce yourself</h3>
+                        <textarea placeholder="Write something about yourself..."></textarea>
+                    </div>
+                    <div className="additional-info-container">
+                        <div className="info-row">
+                            <label>Rental Cost</label>
+                            <button className="info-button">30.0</button>
+                        </div>
+                        <div className="info-row">
+                            <label>Choose Platform</label>
+                            <button className="info-button">Choose</button>
+                        </div>
+                        <div className="info-row">
+                            <label className="bold-label">Platform</label>
+                        </div>
+                    </div>
+                    <div className="save-button-container">
+                        <button className="save-button">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ScheduleProfile;
---
+++ src/Components/Pages/ProfilePage/UserProfilePage/UserProfilePage.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import ProfileSideBar from '../ProfileSideBar/ProfileSideBar';
+import './UserProfilePage.css';
+import classroom_image from 'C:/Users/dante/OneDrive/Desktop/Project/demand-tutor/src/Components/Assets/classroom.png';
+import teacher_image from 'C:/Users/dante/OneDrive/Desktop/Project/demand-tutor/src/Components/Assets/teacher.png';
+import { MdOutlineEmail } from "react-icons/md";
+
+function UserProfilePage() {
+  return (
+    <div className="user-profile-container">
+      <ProfileSideBar />
+      <div className="user-profile-content">
+        <div className="fixed-row">
+          <div className="image-container">
+            <img src={classroom_image} alt="Classroom" className="profile-image" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="user-profile">
+            <img src={teacher_image} alt="Classroom" className="user-profile-image rounded-image" />
+          </div>
+          <div className='profile-details'>
+            <h3>Alexa Rowles</h3>
+            <p>Email: alexa.rowles@example.com</p>
+          </div>
+          <button className="edit-button">Edit</button>
+        </div>
+        <div className="user-profile-name-row">
+          <div className='user-profile-input-bars'>
+            <div className='user-profile-fullname-input-bar'>
+              <label>Full Name</label>
+              <br />
+              <input type="text" placeholder="Full Name" />
+            </div>
+            <div className='user-profile-firstname-input-bar'>
+              <label>First Name</label>
+              <br />
+              <input type="text" placeholder="First Name" />
+            </div>
+          </div>
+        </div>
+        <div className="user-profile-info-row">
+          <div className='user-profile-input-bars'>
+            <div className='user-profile-country-input-bar'>
+              <label>Country</label>
+              <br />
+              <input type="text" placeholder="Country" />
+            </div>
+            <div className='user-profile-gender-input-bar'>
+              <label>Gender</label>
+              <br />
+              <input type="text" placeholder="Gender" />
+            </div>
+          </div>
+        </div>
+        <div className="user-profile-email">
+          <label>My Email Address</label>
+          <div className='email-details'>
+            <MdOutlineEmail />
+            <label>User@gmail.com</label>
+          </div>
+          <p>1 month ago</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserProfilePage;
